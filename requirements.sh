7cddd14998e0b46967883c6ede9ceaaebb9057e3
Joel-Milla <jalejandro.milla@outlook.com>
2025-02-16 22:04:29 +0800

    Add gitignore and work on notebook for image thresholding.

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+conda install conda-forge::opencv -y
+conda install conda-forge::jupyterlab -y
+conda install conda-forge::matplotlib -y
+conda install conda-forge::pandas -y
+conda install conda-forge::numpy -y
+conda install conda-forge::pillow -y
+conda install conda-forge::scipy -y
